d6dbadc1be6be2aed0e218174e8c5159fb062456
gustavo.barbosa <gustavo.barbosa@omni.com.br>
2022-03-21 13:57:04 -0300

    Start Java API

---
+++ src/test/java/br/com/ridgue/argubankapi/ArgubankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ridgue.argubankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgubankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
