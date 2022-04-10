bd660ea7df80af2518bf5f9c42b10b985d38ab5c
gustavo.barbosa <gustavo.barbosa@omni.com.br>
2022-04-10 04:29:40 -0300

    Create client entity

---
+++ src/main/java/br/com/ridgue/argubankapi/gateway/database/entity/Client.java
@@ -0,0 +1,22 @@
+package br.com.ridgue.argubankapi.gateway.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
