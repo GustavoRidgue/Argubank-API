01043b9aa42e1cd9a866d3e59a817e029398ff6f
gustavo.barbosa <gustavo.barbosa@omni.com.br>
2022-07-18 17:19:24 -0300

    Add @SneakyThrows annotation

--- src/main/java/br/com/ridgue/argubankapi/config/security/AuthenticationTokenFilter.java
+++ src/main/java/br/com/ridgue/argubankapi/config/security/AuthenticationTokenFilter.java
@@ -2,7 +2,6 @@ package br.com.ridgue.argubankapi.config.security;
 
 import br.com.ridgue.argubankapi.config.security.service.TokenService;
 import br.com.ridgue.argubankapi.exception.InvalidTokenFormatException;
-import br.com.ridgue.argubankapi.exception.ResourceNotFoundException;
 import br.com.ridgue.argubankapi.gateway.database.entity.Client;
 import br.com.ridgue.argubankapi.gateway.database.repository.ClientRepositoryFacade;
 import br.com.ridgue.argubankapi.http.ws.url.URLMapping;
@@ -14,21 +13,19 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 public class AuthenticationTokenFilter extends OncePerRequestFilter {
 
-    private TokenService tokenService;
-    private ClientRepositoryFacade clientRepositoryFacade;
+    private final TokenService tokenService;
+    private final ClientRepositoryFacade clientRepositoryFacade;
 
     public AuthenticationTokenFilter(TokenService tokenService, ClientRepositoryFacade clientRepositoryFacade) {
         this.tokenService = tokenService;
         this.clientRepositoryFacade = clientRepositoryFacade;
     }
 
-    @SneakyThrows
     @Override
+    @SneakyThrows
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) {
@@ -41,7 +38,8 @@ public class AuthenticationTokenFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String validateToken(HttpServletRequest request) throws InvalidTokenFormatException {
+    @SneakyThrows
+    private String validateToken(HttpServletRequest request) {
         String authorization = request.getHeader("Authorization");
 
         if (authorization == null || authorization.isEmpty() || !authorization.startsWith("Bearer "))
@@ -53,7 +51,8 @@ public class AuthenticationTokenFilter extends OncePerRequestFilter {
         return token;
     }
 
-    private void authenticateToken(String token) throws InvalidTokenFormatException, ResourceNotFoundException {
+    @SneakyThrows
+    private void authenticateToken(String token) {
         Long clientId = tokenService.getUserId(token);
         Client client = clientRepositoryFacade.findById(clientId);
 
